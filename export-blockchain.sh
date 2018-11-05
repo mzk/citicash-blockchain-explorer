e7dd804120d6af1915cccf5123b4523f7bbf1700
Olda Salek <mzk@mozektevidi.net>
2018-11-05 13:21:53 +0100

    script for export blockchain explorer

---
+++ export-blockchain.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+~/mounted2/citicash-blockchain-export --data-dir ~/mounted2/.citicash --output-file ~/mounted2/blockchain.raw.tmp
+md5sum ~/mounted2/blockchain.raw.tmp > ~/mounted2/blockchain.raw.md5sum.txt
+cp ~/mounted2/blockchain.raw.tmp ~/mounted2/blockchain.raw
+cp ~/mounted2/blockchain.raw.md5sum.txt /var/www/blockchain-explorer/www/blockchain.raw.md5sum.txt
+#ln -s ~/mounted2/blockchain.raw /var/www/blockchain-explorer/www/blockchain.raw
