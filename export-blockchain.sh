0f64c3072655c1a1f30c5be847bcb97d18e2014d
Olda Salek <mzk@mozektevidi.net>
2018-12-21 14:54:25 +0100

    export blockchain, copy md5 into server

--- export-blockchain.sh
+++ export-blockchain.sh
@@ -4,3 +4,5 @@ md5sum ~/mounted2/blockchain.raw.tmp > ~/mounted2/blockchain.raw.md5sum.txt
 cp ~/mounted2/blockchain.raw.tmp ~/mounted2/blockchain.raw
 cp ~/mounted2/blockchain.raw.md5sum.txt /var/www/blockchain-explorer/www/blockchain.raw.md5sum.txt
 #ln -s ~/mounted2/blockchain.raw /var/www/blockchain-explorer/www/blockchain.raw
+
+scp /home/ubuntu/mounted2/blockchain.raw.md5sum.txt xx:/home/ubuntu/blockchain.raw.md5sum.txt
