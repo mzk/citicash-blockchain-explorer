4dd244705a7d0381ddf2a6001a88cd98f6eb27d8
Olda Salek <mzk@mozektevidi.net>
2019-01-07 11:45:34 +0100

    lock command

--- app/Models/Console/UploadBlockchainToS3Command.php
+++ app/Models/Console/UploadBlockchainToS3Command.php
@@ -6,12 +6,15 @@ use Aws\Exception\MultipartUploadException;
 use Aws\S3\MultipartUploader;
 use Aws\S3\S3Client;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
 class UploadBlockchainToS3Command extends Command
 {
+
+	use LockableTrait;
 
 	/**
 	 * @var string
@@ -44,16 +47,11 @@ class UploadBlockchainToS3Command extends Command
 
 	protected function execute(InputInterface $input, OutputInterface $output): ?int
 	{
-		$s3Client = new S3Client(
-			[
-				'credentials' => [
-					'key' => $this->s3Key,
-					'secret' => $this->s3Secret,
-				],
-				'region' => 'eu-west-2',
-				'version' => 'latest',
-			]
-		);
+		if (!$this->lock()) {
+			$output->writeln('The command is already running in another process.');
+
+			return 0;
+		}
 
 		$command = '/home/ubuntu/mounted2/citicash-blockchain-export --data-dir /home/ubuntu/mounted2/.citicash --output-file /home/ubuntu/mounted2/blockchain.raw.tmp';
 		$output->writeln($command);
@@ -90,6 +88,8 @@ class UploadBlockchainToS3Command extends Command
 		$copyToAnother = \sprintf('scp /home/ubuntu/mounted2/blockchain.raw.md5sum.txt %s:/home/ubuntu/blockchain.raw.md5sum.txt', $this->citicashIoServer);
 		$copyToAnotherProcess = Process::fromShellCommandline($copyToAnother);
 		$copyToAnotherProcess->run();
+
+		$this->release();
 
 		return 0;
 	}
