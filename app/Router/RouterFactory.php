92422a91bef1c7fe067e2de0931a93546ff93082
Olda Salek <mzk@mozektevidi.net>
2019-01-07 13:36:07 +0100

    redirect blockchain to new server

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -4,6 +4,7 @@ namespace App\Router;
 
 use Nette;
 use Nette\Application\IRouter;
+use Nette\Application\Responses\RedirectResponse;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
 
@@ -15,6 +16,10 @@ class RouterFactory
 	public static function createRouter(): IRouter
 	{
 		$router = new RouteList();
+		$router[] = new Route('blockchain.raw', function () {
+			return new RedirectResponse('https://d1r6e86frnnruv.cloudfront.net/blockchain.raw');
+		});
+
 		$router[] = new Route('index.php', 'Homepage:default', IRouter::ONE_WAY);
 		$router[] = new Route('/block/<hash>', 'Homepage:detail');
 		$router[] = new Route('/height/<height>', 'Homepage:detailByHeight');
